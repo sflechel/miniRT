7f5145b2781a2ade84e5c4058598a9e00d03bccf
sflechel <solalflechelles@gmail.com>
2025-05-27 17:45:46 +0200

    working plane texture

--- bonus/src/shapes/texture.c
+++ bonus/src/shapes/texture.c
@@ -6,14 +6,14 @@
 /*   By: sflechel <sflechel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 10:58:39 by sflechel          #+#    #+#             */
-/*   Updated: 2025/05/27 13:55:53 by sflechel         ###   ########.fr       */
+/*   Updated: 2025/05/27 17:39:24 by sflechel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "math_utils.h"
 #include "shapes.h"
 #include "minirt.h"
-
+#include <stdio.h>
 t_color	plane_get_texture(const t_col *col, const t_plane *plane)
 {
 	t_vec3	u;
@@ -26,8 +26,12 @@ t_color	plane_get_texture(const t_col *col, const t_plane *plane)
 	if (vector_equal(u, (t_vec3){0, 0, 0}) == 1)
 		u = cross_product(plane->normal, (t_vec3){1, 0, 0});
 	v = cross_product(plane->normal, u);
-	u_coord = dot_product(col->pos_world, u) * plane->txtr->len_line;
-	v_coord = dot_product(col->pos_world, v) * plane->txtr->height;
-	txtr_color.rgba = *(plane->txtr->addr + (v_coord * plane->txtr->len_line + u_coord));
+	u_coord = (int)dot_product(col->pos_world, u) % plane->txtr->width / 4;
+	v_coord = (int)dot_product(col->pos_world, v) % plane->txtr->height / 4;
+	if (u_coord < 0)
+		u_coord *= -1;
+	if (v_coord < 0)
+		v_coord *= -1;
+	txtr_color.rgba = *((unsigned int *)plane->txtr->addr + (v_coord * plane->txtr->len_line + u_coord * plane->txtr->bpp / 8));
 	return (txtr_color);
 }
