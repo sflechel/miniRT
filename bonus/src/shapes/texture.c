0930e8ac4df4429ddac3890ad759674a58382bb6
sflechel <solalflechelles@gmail.com>
2025-05-27 17:50:00 +0200

    mirroring teture effect removed

--- bonus/src/shapes/texture.c
+++ bonus/src/shapes/texture.c
@@ -6,14 +6,14 @@
 /*   By: sflechel <sflechel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 10:58:39 by sflechel          #+#    #+#             */
-/*   Updated: 2025/05/27 17:39:24 by sflechel         ###   ########.fr       */
+/*   Updated: 2025/05/27 17:48:14 by sflechel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "math_utils.h"
 #include "shapes.h"
 #include "minirt.h"
-#include <stdio.h>
+
 t_color	plane_get_texture(const t_col *col, const t_plane *plane)
 {
 	t_vec3	u;
@@ -29,9 +29,9 @@ t_color	plane_get_texture(const t_col *col, const t_plane *plane)
 	u_coord = (int)dot_product(col->pos_world, u) % plane->txtr->width / 4;
 	v_coord = (int)dot_product(col->pos_world, v) % plane->txtr->height / 4;
 	if (u_coord < 0)
-		u_coord *= -1;
+		u_coord += plane->txtr->width / 4;
 	if (v_coord < 0)
-		v_coord *= -1;
+		v_coord += plane->txtr->height / 4;
 	txtr_color.rgba = *((unsigned int *)plane->txtr->addr + (v_coord * plane->txtr->len_line + u_coord * plane->txtr->bpp / 8));
 	return (txtr_color);
 }
