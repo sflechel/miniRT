cdb58f32a96d69e421bb8db1bd33c3269820eaaf
Corbio <113342618+Corbiobio@users.noreply.github.com>
2025-05-29 15:31:55 +0200

    fix matrix mult

--- bonus/src/math_utils/matrix.c
+++ bonus/src/math_utils/matrix.c
@@ -6,7 +6,7 @@
 /*   By: edarnand <edarnand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 15:09:25 by edarnand          #+#    #+#             */
-/*   Updated: 2025/05/29 15:29:38 by edarnand         ###   ########.fr       */
+/*   Updated: 2025/05/29 15:31:30 by edarnand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ t_mat_3x3	matrix_mult(t_mat_3x3 m1, t_mat_3x3 m2)
 	new.x.y = m1.x.x * m2.x.y + m1.x.y * m2.y.y + m1.x.z * m2.z.y;
 	new.x.z = m1.x.x * m2.x.z + m1.x.y * m2.y.z + m1.x.z * m2.z.z;
 
-	new.x.x = m1.y.x * m2.x.x + m1.y.y * m2.y.x + m1.y.z * m2.z.x;
-	new.x.y = m1.y.x * m2.x.y + m1.y.y * m2.y.y + m1.y.z * m2.z.y;
-	new.x.z = m1.y.x * m2.x.z + m1.y.y * m2.y.z + m1.y.z * m2.z.z;
+	new.y.x = m1.y.x * m2.x.x + m1.y.y * m2.y.x + m1.y.z * m2.z.x;
+	new.y.y = m1.y.x * m2.x.y + m1.y.y * m2.y.y + m1.y.z * m2.z.y;
+	new.y.z = m1.y.x * m2.x.z + m1.y.y * m2.y.z + m1.y.z * m2.z.z;
 
-	new.x.x = m1.z.x * m2.x.x + m1.z.y * m2.y.x + m1.z.z * m2.z.x;
-	new.x.y = m1.z.x * m2.x.y + m1.z.y * m2.y.y + m1.z.z * m2.z.y;
-	new.x.z = m1.z.x * m2.x.z + m1.z.y * m2.y.z + m1.z.z * m2.z.z;
+	new.z.x = m1.z.x * m2.x.x + m1.z.y * m2.y.x + m1.z.z * m2.z.x;
+	new.z.y = m1.z.x * m2.x.y + m1.z.y * m2.y.y + m1.z.z * m2.z.y;
+	new.z.z = m1.z.x * m2.x.z + m1.z.y * m2.y.z + m1.z.z * m2.z.z;
 	return (new);
 }
