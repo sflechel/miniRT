f5484fe640d87fca4b7e8eef4e00bd9a8cd3bd44
Corbio <113342618+Corbiobio@users.noreply.github.com>
2025-05-29 15:30:12 +0200

    add matrix mult

---
+++ bonus/src/math_utils/matrix.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   matrix.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edarnand <edarnand@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/29 15:09:25 by edarnand          #+#    #+#             */
+/*   Updated: 2025/05/29 15:29:38 by edarnand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "math_utils.h"
+
+t_mat_3x3	matrix_mult(t_mat_3x3 m1, t_mat_3x3 m2)
+{
+	t_mat_3x3	new;
+
+	new.x.x = m1.x.x * m2.x.x + m1.x.y * m2.y.x + m1.x.z * m2.z.x;
+	new.x.y = m1.x.x * m2.x.y + m1.x.y * m2.y.y + m1.x.z * m2.z.y;
+	new.x.z = m1.x.x * m2.x.z + m1.x.y * m2.y.z + m1.x.z * m2.z.z;
+
+	new.x.x = m1.y.x * m2.x.x + m1.y.y * m2.y.x + m1.y.z * m2.z.x;
+	new.x.y = m1.y.x * m2.x.y + m1.y.y * m2.y.y + m1.y.z * m2.z.y;
+	new.x.z = m1.y.x * m2.x.z + m1.y.y * m2.y.z + m1.y.z * m2.z.z;
+
+	new.x.x = m1.z.x * m2.x.x + m1.z.y * m2.y.x + m1.z.z * m2.z.x;
+	new.x.y = m1.z.x * m2.x.y + m1.z.y * m2.y.y + m1.z.z * m2.z.y;
+	new.x.z = m1.z.x * m2.x.z + m1.z.y * m2.y.z + m1.z.z * m2.z.z;
+	return (new);
+}
